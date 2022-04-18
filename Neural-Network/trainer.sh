d94018d2bcacec8f10df094b67904b0ff2923de8
Ben Liggett <67758714+6hl@users.noreply.github.com>
2022-04-18 14:13:33 -0400

    adjusted trainer

--- Neural-Network/trainer.sh
+++ Neural-Network/trainer.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
-for ((a=0; a <= 6; a++))
+for ((s=0; a <= 1; a++))
 do
-  for ((b=1; b <= 3; b++))
+  for ((a=0; a <= 6; a++))
   do
-    python train.py -t $a -r $b -s
-  done
-done
-
-for ((a=0; a <= 6; a++))
-do
-  for ((b=1; b <= 3; b++))
-  do
-    python train.py -t $a -r $b
+    for ((b=1; b <= 3; b++))
+    do
+      if [ $s -eq 0 ]; then
+        python train.py -t $a -r $b -s
+      else
+        python train.py -t $a -r $b
+      fi
+    done
   done
 done
